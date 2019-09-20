24d9feb72345fc16c7eb3bfb59f82f82c1073896
cob16 <public+github@cormacbrady.info>
2019-09-20 15:14:32 +0100

    upgrade to terraform 12 and use terrform cloud backend

--- iam/main.tf
+++ iam/main.tf
@@ -89,16 +89,21 @@ data "aws_iam_policy_document" "manage_personal_mfa" {
       "arn:aws:iam::*:user/$${aws:username}", ## $$ is used here to escape terrfarom parsing this aws var
     ]
 
-    condition = [
-      "${local.condition_if_multi_factor_auth_enabled}",
-    ]
+    condition {
+      test     = "Bool"
+      variable = "aws:MultiFactorAuthPresent"
+
+      values = [
+        "true",
+      ]
+    }
   }
 }
 
 resource "aws_iam_policy" "manage_personal_mfa" {
   name   = "manage_personal_mfa"
   path   = "/"
-  policy = "${data.aws_iam_policy_document.manage_personal_mfa.json}"
+  policy = data.aws_iam_policy_document.manage_personal_mfa.json
 }
 
 data "aws_iam_policy_document" "developers_manage_personal_keys" {
@@ -120,7 +125,7 @@ data "aws_iam_policy_document" "developers_manage_personal_keys" {
 resource "aws_iam_policy" "developers_manage_personal_keys" {
   name   = "developers_manage_personal_keys"
   path   = "/"
-  policy = "${data.aws_iam_policy_document.developers_manage_personal_keys.json}"
+  policy = data.aws_iam_policy_document.developers_manage_personal_keys.json
 }
 
 data "aws_iam_policy_document" "allow_S3_mfa" {
@@ -135,9 +140,14 @@ data "aws_iam_policy_document" "allow_S3_mfa" {
       "*",
     ]
 
-    condition = [
-      "${local.condition_if_multi_factor_auth_enabled}",
-    ]
+    condition {
+      test     = "Bool"
+      variable = "aws:MultiFactorAuthPresent"
+
+      values = [
+        "true",
+      ]
+    }
   }
 
   statement {
@@ -152,16 +162,14 @@ data "aws_iam_policy_document" "allow_S3_mfa" {
       "*",
     ]
 
-    condition = [
-      {
-        test     = "StringNotEquals"
-        variable = "aws:RequestedRegion"
+    condition  {
+      test     = "StringNotEquals"
+      variable = "aws:RequestedRegion"
 
-        values = [
-          "eu-west-2",
-        ]
-      },
-    ]
+      values = [
+        "eu-west-2",
+      ]
+    }
   }
 
   statement {
@@ -182,7 +190,7 @@ data "aws_iam_policy_document" "allow_S3_mfa" {
 resource "aws_iam_policy" "allow_S3_mfa" {
   name   = "allow_S3_mfa"
   path   = "/"
-  policy = "${data.aws_iam_policy_document.allow_S3_mfa.json}"
+  policy = data.aws_iam_policy_document.allow_S3_mfa.json
 }
 
 data "aws_iam_policy_document" "allow_ec2_if_mfa_enabled" {
@@ -200,10 +208,16 @@ data "aws_iam_policy_document" "allow_ec2_if_mfa_enabled" {
       "*",
     ]
 
-    condition = [
-      "${local.condition_if_multi_factor_auth_enabled}",
-      "${local.condition_restrict_region}",
-    ]
+
+    condition {
+      test     = "Bool"
+      variable = "aws:MultiFactorAuthPresent"
+
+      values = [
+        "true",
+      ]
+    }
+
   }
 
   statement {
@@ -217,10 +231,25 @@ data "aws_iam_policy_document" "allow_ec2_if_mfa_enabled" {
       "*",
     ]
 
-    condition = [
-      "${local.condition_if_multi_factor_auth_enabled}",
-      "${local.condition_restrict_region}",
-      {
+    condition {
+      test     = "Bool"
+      variable = "aws:MultiFactorAuthPresent"
+
+      values = [
+        "true",
+      ]
+    }
+
+    condition {
+      test     = "Bool"
+      variable = "aws:MultiFactorAuthPresent"
+
+      values = [
+        "true",
+      ]
+    }
+
+    condition {
         test     = "StringEquals"
         variable = "iam:AWSServiceName"
 
@@ -232,15 +261,14 @@ data "aws_iam_policy_document" "allow_ec2_if_mfa_enabled" {
           "spotfleet.amazonaws.com",
           "transitgateway.amazonaws.com",
         ]
-      },
-    ]
+    }
   }
 }
 
 resource "aws_iam_policy" "allow_ec2_if_mfa_enabled" {
   name   = "allow_ec2_if_mfa_enabled"
   path   = "/"
-  policy = "${data.aws_iam_policy_document.allow_ec2_if_mfa_enabled.json}"
+  policy = data.aws_iam_policy_document.allow_ec2_if_mfa_enabled.json
 }
 
 # admin policys
@@ -257,16 +285,21 @@ data "aws_iam_policy_document" "allow_admin_if_mfa" {
       "*",
     ]
 
-    condition = [
-      "${local.condition_if_multi_factor_auth_enabled}",
-    ]
+    condition {
+      test     = "Bool"
+      variable = "aws:MultiFactorAuthPresent"
+
+      values = [
+        "true",
+      ]
+    }
   }
 }
 
 resource "aws_iam_policy" "allow_admin_if_mfa" {
   name   = "allow_admin_if_mfa"
   path   = "/"
-  policy = "${data.aws_iam_policy_document.allow_admin_if_mfa.json}"
+  policy = data.aws_iam_policy_document.allow_admin_if_mfa.json
 }
 
 data "aws_iam_policy_document" "assume_role_to_admin" {
@@ -279,16 +312,21 @@ data "aws_iam_policy_document" "assume_role_to_admin" {
       "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/admin",
     ]
 
-    condition = [
-      "${local.condition_if_multi_factor_auth_enabled}",
-    ]
+    condition {
+      test     = "Bool"
+      variable = "aws:MultiFactorAuthPresent"
+
+      values = [
+        "true",
+      ]
+    }
   }
 }
 
 resource "aws_iam_policy" "assume_role_to_admin" {
   name   = "assume_role_to_admin"
   path   = "/"
-  policy = "${data.aws_iam_policy_document.assume_role_to_admin.json}"
+  policy = data.aws_iam_policy_document.assume_role_to_admin.json
 }
 
 # admin assume role
@@ -305,18 +343,18 @@ data "aws_iam_policy_document" "admin_assume_role_policy" {
 
 resource "aws_iam_role" "admin_assume_role" {
   name               = "admin"
-  assume_role_policy = "${data.aws_iam_policy_document.admin_assume_role_policy.json}"
+  assume_role_policy = data.aws_iam_policy_document.admin_assume_role_policy.json
 }
 
 resource "aws_iam_policy_attachment" "admin_assume_role_policy" {
   name       = "admin_assume_role_policy"
-  roles      = ["${aws_iam_role.admin_assume_role.name}"]
-  policy_arn = "${aws_iam_policy.allow_admin_if_mfa.arn}"
+  roles      = [aws_iam_role.admin_assume_role.name]
+  policy_arn = aws_iam_policy.allow_admin_if_mfa.arn
 }
 
 # developer group policy-attachments
 resource "aws_iam_group_policy_attachment" "developers_iam_read_only_access" {
-  group      = "${aws_iam_group.developer.name}"
+  group      = aws_iam_group.developer.name
   policy_arn = "arn:aws:iam::aws:policy/IAMReadOnlyAccess"
 }
 
@@ -327,38 +365,38 @@ resource "aws_iam_group_policy_attachment" "developers_iam_read_only_access" {
 # }
 
 resource "aws_iam_group_policy_attachment" "developers_iam_User_change_password" {
-  group      = "${aws_iam_group.developer.name}"
+  group      = aws_iam_group.developer.name
   policy_arn = "arn:aws:iam::aws:policy/IAMUserChangePassword"
 }
 
 resource "aws_iam_group_policy_attachment" "developers_iam_user_ssh_keys" {
-  group      = "${aws_iam_group.developer.name}"
+  group      = aws_iam_group.developer.name
   policy_arn = "arn:aws:iam::aws:policy/IAMUserSSHKeys"
 }
 
 resource "aws_iam_group_policy_attachment" "developers_manage_personal_keys" {
-  group      = "${aws_iam_group.developer.name}"
-  policy_arn = "${aws_iam_policy.developers_manage_personal_keys.arn}"
+  group      = aws_iam_group.developer.name
+  policy_arn = aws_iam_policy.developers_manage_personal_keys.arn
 }
 
 resource "aws_iam_group_policy_attachment" "developers_manage_personal_mfa" {
-  group      = "${aws_iam_group.developer.name}"
-  policy_arn = "${aws_iam_policy.manage_personal_mfa.arn}"
+  group      = aws_iam_group.developer.name
+  policy_arn = aws_iam_policy.manage_personal_mfa.arn
 }
 
 resource "aws_iam_group_policy_attachment" "developers_allow_s3_mfa" {
-  group      = "${aws_iam_group.developer.name}"
-  policy_arn = "${aws_iam_policy.allow_S3_mfa.arn}"
+  group      = aws_iam_group.developer.name
+  policy_arn = aws_iam_policy.allow_S3_mfa.arn
 }
 
 resource "aws_iam_group_policy_attachment" "developers_allow_ec2_if_mfa_enabled" {
-  group      = "${aws_iam_group.developer.name}"
-  policy_arn = "${aws_iam_policy.allow_ec2_if_mfa_enabled.arn}"
+  group      = aws_iam_group.developer.name
+  policy_arn = aws_iam_policy.allow_ec2_if_mfa_enabled.arn
 }
 
 # admin group policy-attachments
 
 resource "aws_iam_group_policy_attachment" "assume_role_to_admin" {
-  group      = "${aws_iam_group.admin.name}"
-  policy_arn = "${aws_iam_policy.assume_role_to_admin.arn}"
+  group      = aws_iam_group.admin.name
+  policy_arn = aws_iam_policy.assume_role_to_admin.arn
 }
--- main.tf
+++ main.tf
@@ -1,19 +1,12 @@
 provider "aws" {
-  region  = "eu-west-2"
-  version = "~> 1.56"
+  region = "eu-west-2"
 }
 
-# comment this out on first run
 terraform {
-  backend "s3" {
-    encrypt        = true
-    bucket         = "ceb-terraform-remote-state-storage-s3"
-    dynamodb_table = "ceb-terraform-state-lock-dynamo" # locks changes when running in a team enviroment
-    region         = "eu-west-2"
-    key            = ".terraform/terraform.tfstate"
-  }
+  backend "remote" {}
 }
 
 module "iam" {
   source = "./iam"
 }
+
